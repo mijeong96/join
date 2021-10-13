416651807a9e74b6960e08480d59a2ee199f9548
mijeong96 <alwjdekdma2@gmail.com>
2021-10-13 16:43:55 +0900

    6.이메일인증

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 $("input[type=submit]").on("click",function(e){
     e.preventDefault();
 
+    //isTxt인증함수의 return값이 false이면 전송을 막음
+    //검사할 input의 name값과 최소글자수를 인수로 전달
     if(!isTxt("userid",5))e.preventDefault();
-    isTxt("userid",5);
-    isTxt("comment",20);
+    if(!isTxt("comment",20))e.preventDefault();
+
+    //
+    if(!isEmail("email"))e.preventDefault();
 
 });
 
@@ -26,4 +30,24 @@ function isTxt(name, len){
         );
     }
     return false;
+}
+
+//이메일 인증함수 정의
+function isEmail(name){
+    //input창에 값을 txt로 전달받음
+    let txt = $("[name="+name+"]").val();
+
+    //만약 이메일 인풋의 텍스트에 @문자가 있으면
+    if(/@/.test(txt)){
+        //에러 메시지를 제거하고 true를 반환
+        $("[name="+name+"]").parent().find("p").remove();
+        return true;
+    }else{//만약 @문자가 없으면
+        //중복 메시지 출력 방지를 위해서 기존 메시지 제거하고
+        //다시 새로운 에러메시지 출력하고 false반환
+        $("[name="+name+"]").parent().find("p").remove();
+        $("[name="+name+"]").parent().append(
+            "<p>@를 포함한 전체 메일주소를 입력하세요.</p>"
+        )
+    }
 }
